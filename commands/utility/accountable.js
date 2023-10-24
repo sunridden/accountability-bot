45d04cd1e366c5218daf2095be866a16d7fe5df1
sunridden <kevinszeto9449@gmail.com>
2023-10-23 17:09:34 -0700

    Change text input

--- commands/utility/accountable.js
+++ commands/utility/accountable.js
@@ -3,13 +3,13 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('accountable')
-        .setDescription('Set a new habit to be reminded of with the format - name, message, time')
+        .setDescription('Set new habits to be reminded of')
         .addStringOption(option =>
-            option.setName('input')
-                .setDescription('The text that echoes')
+            option.setName('habit')
+                .setDescription('Enter habits here')
                 .setRequired(true)),
     async execute(interaction) {
-        const textInput = interaction.options.getString('input');
+        const textInput = interaction.options.getString('habit');
         if (!textInput) {
             return interaction.reply('Please enter the required text prompts');
         } else {
