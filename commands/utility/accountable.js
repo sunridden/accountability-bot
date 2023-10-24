7c53eaa33067f4477097a53827f79fa615bb2666
sunridden <kevinszeto9449@gmail.com>
2023-10-23 17:06:08 -0700

    Require text input for accountable function

--- commands/utility/accountable.js
+++ commands/utility/accountable.js
@@ -6,9 +6,14 @@ module.exports = {
         .setDescription('Set a new habit to be reminded of with the format - name, message, time')
         .addStringOption(option =>
             option.setName('input')
-                .setDescription('The text that echoes')),
+                .setDescription('The text that echoes')
+                .setRequired(true)),
     async execute(interaction) {
         const textInput = interaction.options.getString('input');
-        await interaction.reply(`You entered: ${textInput}`);
+        if (!textInput) {
+            return interaction.reply('Please enter the required text prompts');
+        } else {
+            await interaction.reply(`You entered: ${textInput}`);
+        }
     },
 };
